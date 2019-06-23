f1f40a78de958155d9a473de9f5e47230c49ba37
TokunaKimochi <tokunakimochi@gmail.com>
2019-06-23 20:00:19 +0900

    Update 2019-06-23T11:00:16.342Z

--- loadjsrc.js
+++ loadjsrc.js
@@ -1,17 +1,17 @@
 // https://github.com/muicss/loadjs
 'use strict';
 
-loadjs([ '//tokunakimochi.github.io/tokunakimochi.blog.jp/tokunakimochi-blog-jp.min.css'
-         , '//tokunakimochi.github.io/prism/themes/prism-bootstrap.css'
-         , '//tokunakimochi.github.io/prism/plugins/line-numbers/prism-bootstrap-line-numbers.css'
-         , '//tokunakimochi.github.io/prism/plugins/line-highlight/prism-bootstrap-line-highlight.css'
-         , '//tokunakimochi.github.io/Nivo-Lightbox/nivo-lightbox.min.css' ], 'css');
+loadjs([ 'https://tokunakimochi.github.io/tokunakimochi.blog.jp/tokunakimochi-blog-jp.min.css'
+         , 'https://tokunakimochi.github.io/prism/themes/prism-bootstrap.css'
+         , 'https://tokunakimochi.github.io/prism/plugins/line-numbers/prism-bootstrap-line-numbers.css'
+         , 'https://tokunakimochi.github.io/prism/plugins/line-highlight/prism-bootstrap-line-highlight.css'
+         , 'https://tokunakimochi.github.io/Nivo-Lightbox/nivo-lightbox.min.css' ], 'css');
 
-loadjs('//tokunakimochi.github.io/tokunakimochi.blog.jp/tokunakimochi-blog-jp.iife.min.js', 'pre');
+loadjs('https://tokunakimochi.github.io/tokunakimochi.blog.jp/tokunakimochi-blog-jp.iife.min.js', 'pre');
 
-loadjs([ '//ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js'
-         , '//ajax.googleapis.com/ajax/libs/hammerjs/2.0.8/hammer.min.js'
-         , '//ajax.googleapis.com/ajax/libs/webfont/1.6.26/webfont.js' ], 'dep');
+loadjs([ 'https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js'
+         , 'https://ajax.googleapis.com/ajax/libs/hammerjs/2.0.8/hammer.min.js'
+         , 'https://ajax.googleapis.com/ajax/libs/webfont/1.6.26/webfont.js' ], 'dep');
 
 loadjs.ready(['css', 'pre', 'dep'], {
   success: function() {
@@ -21,6 +21,6 @@ loadjs.ready(['css', 'pre', 'dep'], {
       }
     });
     loadjs([ '//parts.blog.livedoor.jp/js/design.js'
-             , '//tokunakimochi.github.io/tokunakimochi.blog.jp/tokunakimochi-blog-jp.min.js' ]);
+             , 'https://tokunakimochi.github.io/tokunakimochi.blog.jp/tokunakimochi-blog-jp.min.js' ]);
   }
 });
