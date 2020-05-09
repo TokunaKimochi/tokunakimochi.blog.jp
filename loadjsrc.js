5e2a0c79f03faf62c83fe120a72701e4c232fc5c
TokunaKimochi <tokunakimochi@gmail.com>
2020-05-09 19:46:47 +0900

    Update 2020-05-09T10:46:44.032Z

--- loadjsrc.js
+++ loadjsrc.js
@@ -3,9 +3,6 @@
 
 loadjs([ 'https://cdn.jsdelivr.net/npm/sanitize.css@10.0.0/sanitize.min.css'
          , 'https://tokunakimochi.github.io/tokunakimochi.blog.jp/tokunakimochi-blog-jp.min.css'
-         , 'https://tokunakimochi.github.io/prism/themes/prism-bootstrap.css'
-         , 'https://tokunakimochi.github.io/prism/plugins/line-numbers/prism-bootstrap-line-numbers.css'
-         , 'https://tokunakimochi.github.io/prism/plugins/line-highlight/prism-bootstrap-line-highlight.css'
          , 'https://tokunakimochi.github.io/Nivo-Lightbox/nivo-lightbox.min.css' ], 'css');
 
 loadjs('https://tokunakimochi.github.io/tokunakimochi.blog.jp/tokunakimochi-blog-jp.iife.min.js', 'pre');
