b3c9bfead7961c775223d37d7f10dae3ae9d2079
TokunaKimochi <tokunakimochi@gmail.com>
2020-06-07 18:29:24 +0900

    引き続きスムーススクロールで試行錯誤

--- scripts/watch-jumps-and-scroll.js
+++ scripts/watch-jumps-and-scroll.js
@@ -7,16 +7,19 @@ jQuery(window).on('pageshow', function() {
   jQuery('a[href^="#"]').on('click', function(e) {
     e.preventDefault();
     var href = jQuery(this).attr('href');
-    jump(href);
+    setTimeout(jump, 0, href);
   });
 
-  function jump(str) {
-    // `#' をカット
-    var id = str.slice(1);
+  function jump(sharpStr) {
+    var str = sharpStr.slice(1);
     // getElementById に空文字列を渡すと警告が出る
-    if (!id) return;
-    var elem = document.getElementById(id);
-    if (!elem) return;
-    elem.scrollIntoView({behavior: "smooth"});
+    if (!str) return;
+    var dom = document.getElementById(str);
+    // 存在確認 IE 非対応
+    if (!dom.isConnected) return;
+    var rect = dom.getBoundingClientRect();
+    var scrollTop = window.pageYOffset || document.documentElement.scrollTop;
+    var top = rect.top + scrollTop;
+    jQuery('html, body').animate({ scrollTop: top }, 500, 'swing');
   }
 });
