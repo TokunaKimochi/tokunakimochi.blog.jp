453a6e1c94d6ef55c723231b036374fe9385e7c0
TokunaKimochi <tokunakimochi@gmail.com>
2020-06-10 01:16:29 +0900

    引き続きスムーススクロールで試行錯誤

--- scripts/watch-jumps-and-scroll.js
+++ scripts/watch-jumps-and-scroll.js
@@ -1,24 +1,26 @@
-jQuery(window).on('pageshow', () => {
+jQuery(window).on('pageshow', function() {
   'use strict';
+  setTimeout(watch, 0);
 
-  const hash = window.location.hash;
-  setTimeout(jump, 0, hash);
+  function watch() {
+    const jump = sharpStr => {
+      const str = sharpStr.slice(1);
+      // getElementById に空文字列を渡すと警告が出る
+      if (!str) return;
+      const dom = document.getElementById(str);
+      // 存在確認 IE 非対応
+      if (!dom.isConnected) return;
+      const top = jQuery(dom).offset().top;
+      jQuery('html, body').animate({ scrollTop: top }, 500, 'swing');
+    };
 
-  jQuery('a[href^="#"]').on('click', e => {
-    e.preventDefault();
-    const href = jQuery(e.currentTarget).attr('href');
-    jump(href);
-  });
+    const hash = window.location.hash;
+    if (hash) jump(hash);
 
-  function jump(sharpStr) {
-    const str = sharpStr.slice(1);
-    // getElementById に空文字列を渡すと警告が出る
-    if (!str) return;
-    const dom = document.getElementById(str);
-    // 存在確認 IE 非対応
-    if (!dom.isConnected) return;
-    const top = jQuery(dom).offset().top;
-    jQuery('html, body').animate({ scrollTop: top }, 500, 'swing');
+    jQuery('a[href^="#"]').on('click', e => {
+      e.preventDefault();
+      const href = jQuery(e.currentTarget).attr('href');
+      jump(href);
+    });
   }
-
 });
