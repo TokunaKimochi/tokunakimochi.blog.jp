a7590db51ffb435506a7c868d6944d8bd3fa6e25
TokunaKimochi <tokunakimochi@gmail.com>
2022-05-04 16:08:48 +0900

    横スクロールが必要な Prism.js 下のコードを横方向に引き伸ばすボタンを実装

--- scripts/prism-helper.js
+++ scripts/prism-helper.js
@@ -14,12 +14,81 @@
       return text;
     };
 
+    var stretchDiv = function() {
+      var div, prevElem, nextElem, divHeight, divMarginTop, prevMarginBottom, divMarginBottom, nextMarginTop,
+          parentElem, parentPaddingBottom, windowWidth, divWidth, offset, myPx, familyPx;
+      div = $(this).parents('div.named').eq(0);
+      prevElem = div.prev();
+      nextElem = div.next();
+      // position: static => absolute
+      if (! div.hasClass('stretched')){
+        divHeight = div.outerHeight(true);
+        // 長子の場合は margin-top の調整は不要
+        if (prevElem.length){
+          divMarginTop = Number(div.css('margin-top').replace('px', ''));
+          prevMarginBottom = Number(prevElem.css('margin-bottom').replace('px', ''));
+          div.data('mypx', divMarginTop);
+          // マージンの相殺を勘案
+          if (prevMarginBottom >= divMarginTop){
+            div.css('margin-top', 0);
+          }
+          else {
+            div.css('margin-top', divMarginTop - prevMarginBottom);
+          }
+        }
+
+        if (nextElem.length){
+          divMarginBottom = Number(div.css('margin-bottom').replace('px', ''));
+          nextMarginTop = Number(nextElem.css('margin-top').replace('px', ''));
+          div.data('familypx', nextMarginTop);
+          // マージンの相殺を勘案
+          if (divMarginBottom >= nextMarginTop){
+            nextElem.css('margin-top', divHeight);
+          }
+          else {
+            nextElem.css('margin-top', divHeight + nextMarginTop - divMarginBottom);
+          }
+        }
+        else {
+          parentElem = div.parent();
+          parentPaddingBottom = Number(parentElem.css('padding-bottom').replace('px', ''));
+          div.data('familypx', parentPaddingBottom);
+          parentElem.css('padding-bottom', divHeight + parentPaddingBottom);
+        }
+        div.addClass('stretched');
+        div.css('max-width', document.body.clientWidth);
+        // Returns width of browser viewport
+        windowWidth = $(window).width();
+        divWidth = div.outerWidth(true);
+        offset = (windowWidth - divWidth) / 2;
+        div.offset({left: offset});
+      }
+      // position: absolute => static
+      else {
+        if (prevElem.length){
+          myPx = Number(div.data('mypx'));
+          div.css('margin-top', myPx);
+        }
+
+        familyPx = Number(div.data('familypx'));
+        if (nextElem.length){
+          nextElem.css('margin-top', familyPx);
+        }
+        else {
+          div.parent().css('padding-bottom', familyPx);
+        }
+        div.removeClass('stretched');
+        div.css('max-width', '');
+        div.css('left', '');
+      }
+    };
+
     var main = function() {
       return dom.each(function(index) {
 
         var jqueryObj = $(this);
         var txtArr = [], splitArr = [], lineNumArr = [], highlightLineArr = [], titleArr = [], codeNoClassArr = [];
-        var numberingClassname, txt, anchor, h3Id, pre, code, specifyLng, i, classname, score, flag, title, codeNoClass;
+        var numberingClassname, txt, anchor, h3Id, stretchButton, pre, code, specifyLng, i, classname, score, flag, title, codeNoClass;
 
         if (jqueryObj.is('h3')) {
           numberingClassname = 'code-No_' + index;
@@ -29,6 +98,7 @@
             txt = getNodeText(jqueryObj[0]);
             anchor = jqueryObj.children('a');
             h3Id = jqueryObj.attr('id');
+            stretchButton = '';
             // エスケープ
             if (/^\s*'/.test(txt)) {
               txt = txt.slice(1);
@@ -38,7 +108,8 @@
             else if ((txtArr = txt.match(/^\s*\+\+([a-zA-Z0-9]+)\s*$/)) !== null) {
               pre.addClass( numberingClassname );
               jqueryObj.replaceWith( function() {
-                return $('<h3 id="' + h3Id + '" class="filename ' + numberingClassname + '">' + txtArr[1] + '</h3>').prepend(anchor);
+                return $('<h3 id="' + h3Id + '" class="filename ' + numberingClassname + '">' + txtArr[1] +
+                         '</h3>').prepend(anchor);
               });
               specifyLng = 'language-' + txtArr[1];
               pre.addClass(specifyLng + ' line-numbers');
@@ -72,6 +143,7 @@
                 // ClassName 追加のオプション群
                 else if (/--scroll-x|-S/.test(splitArr[i])) {
                   pre.addClass('scroll-x');
+                  stretchButton = '<button class="stretch-x"></button>';
                 }
                 else if (/--jp-monospace|-J/.test(splitArr[i])) {
                   pre.addClass('jp-monospace');
@@ -86,7 +158,8 @@
               if (titleArr.length) {
                 title = titleArr.join(' ');
                 jqueryObj.replaceWith( function() {
-                  return $('<h3 id="' + h3Id + '" class="filename ' + numberingClassname + '">' + title + '</h3>').prepend(anchor);
+                  return $('<h3 id="' + h3Id + '" class="filename ' + numberingClassname + '">' + title +
+                           stretchButton + '</h3>').prepend(anchor);
                 });
               }
               else {
@@ -123,6 +196,7 @@
     Prism.highlightAll();
     dom = $('pre');
     main();
+    $(document).on('click', '.stretch-x', stretchDiv);
 
     return this;
   };
