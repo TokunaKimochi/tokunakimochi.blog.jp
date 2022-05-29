f58cdecfe15bc1e33171899148f82ee906268a53
TokunaKimochi <tokunakimochi@gmail.com>
2022-05-29 17:03:57 +0900

    hatena-blogcard-helper.js に渡される uri はエンコード済みと仮定

--- scripts/hatena-blogcard-helper.js
+++ scripts/hatena-blogcard-helper.js
@@ -7,13 +7,13 @@
 
         var jqueryObj = $(this);
         var txt = jqueryObj.parent().text().trim();
-        var permanentLink = jqueryObj.attr('href').trim();
-        if (txt === permanentLink) {
+        var permanentLinkEnc = jqueryObj.attr('href').trim();
+        var permanentLinkDec = decodeURIComponent( permanentLinkEnc );
+        if (txt === permanentLinkDec) {
           var title = jqueryObj.attr('title') || 'はてなブログカード';
           var re = /(?:https?:)?\/\/(?:www\.)?(?:[^./]+\.)?hatena/;
-          var classNames = re.test(permanentLink) ? 'hatena-blogcard hbc-for-hatena' : 'hatena-blogcard hbc-for-international';
-          permanentLink = encodeURIComponent( permanentLink );
-          jqueryObj.replaceWith('<iframe src="https://hatenablog-parts.com/embed?url=' + permanentLink +
+          var classNames = re.test(permanentLinkDec) ? 'hatena-blogcard hbc-for-hatena' : 'hatena-blogcard hbc-for-international';
+          jqueryObj.replaceWith('<iframe src="https://hatenablog-parts.com/embed?url=' + permanentLinkEnc +
                                 '" title="' + title + '" class="' + classNames + '" scrolling="no" frameborder="0"></iframe>');
         }
       });
